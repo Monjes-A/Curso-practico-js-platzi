752913bfaf80158b38b08e7c41ebf196a2ee8788
~Alveiro~ <jesusmonsalve1414@gmail.com>
2022-03-26 12:42:18 -0400

    ya con el reto 3 cumplido y bien analizado mejorado para mi comprencion del codigo y personalizando un poco los mensajes

--- Descuentos.js
+++ Descuentos.js
@@ -1,21 +1,52 @@
 const ResultP = document.getElementById("ResultadoP");
 const coupons = [
-	"JuanDC_es_Batman",
-	"pero_no_le_digas_a_nadie",
-	"es_un_secreto",
+	{
+		name: "aprendiendo",
+		discount: 15,
+	},
+	{
+		name: "Monjes",
+		discount: 30,
+	},
+	{
+		name: "es_un_secreto",
+		discount: 25,
+	},
 ];
+const resultP = document.getElementById("ResultP");
 
 function calcularPrecioConDescuento(PrecioOriginal, Descuento) {
 	return (PrecioOriginal * (100 - Descuento)) / 100;
 }
 
 function onClickButtonPriceDiscount() {
-    const preceSinvalue = document.getElementById("Precio");
+	const preceSinvalue = document.getElementById("Precio");
 	const priceValue = Number(preceSinvalue.value);
 	const inputCoupon = document.getElementById("InputCoupon");
 	const couponValue = inputCoupon.value;
+    resultP.innerText = "El Cupon no es valido $ el precio sigue siendo $" + priceValue;
 
-	let descuento;
+
+
+/* 	const isCouponValueValid = function (coupon) {
+		return coupon.name === couponValue;
+	}; */
+
+	const userCoupon = coupons.find(coupon => coupon.name === couponValue);
+
+	if (!userCoupon) {
+    alert("El cupón " + couponValue + "no es válido");
+}
+
+else {
+    const descuento = userCoupon.discount;
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    // resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    resultP.innerText = `El precio es de ${priceValue} y con el descuento del ${descuento}% es de: ${precioConDescuento}$`;
+
+}
+}
+	/* 	let descuento;
 	if (!coupons.includes(couponValue)) {
 		alert("El cupón " + couponValue + " no es válido");
 	} else if (couponValue === "JuanDC_es_Batman") {
@@ -24,7 +55,7 @@ function onClickButtonPriceDiscount() {
 		descuento = 30;
 	} else if (couponValue === "es_un_secreto") {
 		descuento = 25;
-	}
+	} */
 
 	/*   switch(couponValue) {
     case coupons[0]: // "JuanDC_es_Batman"
@@ -36,7 +67,7 @@ function onClickButtonPriceDiscount() {
     case coupons[2]: // "es_un_secreto"
       descuento = 25;
     break;
- */
+
 	const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 	ResultP.innerText = `El Precio a Pagar es de ${precioConDescuento}`;
-}
+*/
